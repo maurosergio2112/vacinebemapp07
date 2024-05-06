56ec4a2a8680226cfcb6b3f68666d5619af8059b
maurosergioifb2021 <57821509+maurosergio2112@users.noreply.github.com>
2024-05-06 10:15:33 -0300

    atualização das dependencias

--- App.js
+++ App.js
@@ -1,26 +1,26 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { AppRegistry } from "react-native";
+
 import HomeScreen from "./screens/HomeScreen";
 import QuestionarioScreen from "./screens/QuestionarioScreen";
 import AbasVacinas from "./screens/Abasvacinas";
 import CartaoVacinaScreen from "./screens/CartaoVacinaScreen";
 import DadosdasVacinasScreen from "./screens/DadosdasVacinasScreen";
-import { AppRegistry } from "react-native";
 import PerfilScreen from "./screens/PerfilScreen";
 import RegistroUsuarioScreen from "./screens/RegistroUsuarioScreen";
 
 const Stack = createStackNavigator();
+// Registro do componente App
+AppRegistry.registerComponent("blank", () => App);
 
 // Registro do componente App
-AppRegistry.registerComponent("main", () => App);
-
 const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="RegistroUsuarioScreen">
-      
-      <Stack.Screen
+        <Stack.Screen
           name="RegistroUsuarioScreen"
           component={RegistroUsuarioScreen}
           options={{ title: "RegistroUsuarioScreen" }}
@@ -52,7 +52,7 @@ const App = () => {
           options={{ title: "DadosdasVacinasScreen" }}
         />
 
-<Stack.Screen
+        <Stack.Screen
           name="PerfilScreen"
           component={PerfilScreen}
           options={{ title: "PerfilScreen" }}
@@ -61,5 +61,7 @@ const App = () => {
     </NavigationContainer>
   );
 };
+
+
 
 export default App;
